e8d81ce83d906513afd67a53bc20af0a3ba3905d
neshvig10 <s.vigneshwaran.cse21@iitbhu.ac.in>
2024-05-15 19:13:11 +0530

    Initial Commit

---
+++ neshkart/src/main/java/neshkart/com/example/neshkart/NeshkartApplication.java
@@ -0,0 +1,13 @@
+package neshkart.com.example.neshkart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeshkartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeshkartApplication.class, args);
+	}
+
+}
---
+++ neshkart/src/test/java/neshkart/com/example/neshkart/NeshkartApplicationTests.java
@@ -0,0 +1,13 @@
+package neshkart.com.example.neshkart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeshkartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
